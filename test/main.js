bdcbe1ad959bc58e3dbe6ade087cd1c3e1bd69ff
Jacopo Scazzosi <jacoscaz@users.noreply.github.com>
2018-09-03 10:26:51 +0100

    Removes asynctools, changes client.js dep to refer to specific commit, bumps up version (#2)
    * adds jsconfig file for visual studio code
    * removes asynctools as a dependency (#1)
    * references a specific commit of bi's fork of client.js as a dependency (as opposed to the a branch)
    * bumps up to version 0.0.2

--- test/main.js
+++ test/main.js
@@ -1,20 +1,26 @@
 
 'use strict';
 
-const utils = require('quadstore/lib/utils');
+const utils = require('../lib/utils');
 const nanoid = require('nanoid');
 const memdown = require('memdown');
 const dataFactory = require('rdf-data-model');
 const RdfStore = require('quadstore').RdfStore;
-const asynctools = require('asynctools');
 
 const SparqlEngine = require('..');
 
 describe('Main', () => {
 
+  beforeEach(async () => {
+    this.store = new RdfStore(nanoid(), { dataFactory, db: memdown });
+    await utils.waitForEvent(this.store, 'ready');
+  });
+
+  afterEach(async () => {
+    await this.store.close();
+  });
+
   it('Should work', async () => {
-    const store = new RdfStore(nanoid(), { dataFactory, db: memdown });
-    await asynctools.onEvent(store, 'ready');
     const quads = [];
     for (let i = 0; i < 20; i++) {
       quads.push(dataFactory.quad(
@@ -24,14 +30,13 @@ describe('Main', () => {
         dataFactory.namedNode('http://ex.com/g' + i)
       ));
     }
-    await store.put(quads);
+    await this.store.put(quads);
     const query = 'SELECT *  WHERE { GRAPH ?g { <http://ex.com/s1> ?p ?o } }';
-    const engine = new SparqlEngine(store);
+    const engine = new SparqlEngine(this.store);
     const sparqlIterator = engine.query(query);
     const queryStream = utils.createIteratorStream(sparqlIterator);
     const results = await utils.streamToArray(queryStream);
-    console.log(results);
-    await store.close();
+    console.log(results);    
   });
 
 });
